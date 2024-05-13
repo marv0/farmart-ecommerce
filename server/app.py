87a2b94adff6a197cea07068bc18848c69958971
victor <victormuhoro7@gmail.com>
2024-05-13 19:12:30 +0300

    Created the models

--- server/app.py
+++ server/app.py
@@ -1,23 +1,46 @@
-#!/usr/bin/env python3
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import JWTManager, create_access_token
+from werkzeug.security import generate_password_hash, check_password_hash
 
-# Standard library imports
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'  # Change this to a secure secret key
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://username:password@localhost/database_name'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# Remote library imports
-from flask import request
-from flask_restful import Resource
+db = SQLAlchemy(app)
+jwt = JWTManager(app)
 
-# Local imports
-from config import app, db, api
-# Add your model imports
+# User model
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
 
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
 
-# Views go here!
+# Routes
+@app.route('/register', methods=['POST'])
+def register():
+    data = request.get_json()
+    hashed_password = generate_password_hash(data['password'], method='sha256')
+    new_user = User(username=data['username'], password=hashed_password)
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({'message': 'User created successfully!'}), 201
 
-@app.route('/')
-def index():
-    return '<h1>Project Server</h1>'
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    user = User.query.filter_by(username=data['username']).first()
 
+    if not user or not check_password_hash(user.password, data['password']):
+        return jsonify({'message': 'Invalid username or password'}), 401
+
+    access_token = create_access_token(identity=user.id)
+    return jsonify({'access_token': access_token}), 200
 
 if __name__ == '__main__':
-    app.run(port=5555, debug=True)
-
+    app.run(debug=True)
